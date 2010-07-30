4c86d9104062c53edad9e02e226cad29d55197bb
Gabe Berke-Williams <gbw@brandeis.edu>
2010-07-29 18:11:39 -0700

    Refactor pages controller spec to use before(:each)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,41 +2,47 @@ require 'spec_helper'
 
 describe PagesController do
   render_views # force rspec to actually render views, not just test the actions
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App |"
+  end
 
   describe "GET 'home'" do
-    it "should be successful" do
+    before(:each) do
       get 'home'
+    end
+    it "should be successful" do
       response.should be_success
     end
 
     it "should have the right title" do
-      get 'home'
       response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample App | Home")
+        :content => "#{@base_title} Home")
     end
   end
 
   describe "GET 'contact'" do
-    it "should be successful" do
+    before(:each) do
       get 'contact'
+    end
+    it "should be successful" do
       response.should be_success
     end
     it "should have the right title" do
-      get 'contact'
       response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample App | Contact")
+        :content => "#{@base_title} Contact")
     end
   end
 
   describe "GET 'about'" do
-    it "should be successful" do
+    before(:each) do
       get 'about'
+    end
+    it "should be successful" do
       response.should be_success
     end
     it "should have the right title" do
-      get 'about'
       response.should have_selector("title",
-        :content => "Ruby on Rails Tutorial Sample App | About")
+        :content => "#{@base_title} About")
     end
   end
 end
