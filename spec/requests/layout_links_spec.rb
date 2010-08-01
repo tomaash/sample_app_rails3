939c63eea41f6c265e7d84073be01df458277b1e
Gabe Berke-Williams <gbw@brandeis.edu>
2010-07-31 18:05:41 -0700

    Test for correct links in layout

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -25,4 +25,19 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => "Sign up")
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should be_success
+    click_link "Contact"
+    response.should be_success
+    click_link "Home"
+    response.should be_success
+    click_link "Sign up now!"
+    response.should be_success
+  end
+
 end
