b1a8f1495cd20949c21c2b36bae7f950451447a3
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-01 21:07:09 -0700

    Validate formate and uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,12 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+  # http://railstutorial.org/chapters/modeling-and-viewing-users-one#code:validates_format_of_email
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name, :presence => true,
-                   :length => { :maximum => 50 }
-  validates :email, :presence => true
+                   :length   => { :maximum => 50 }
+  validates :email, :presence => true,
+                    :format   => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,4 +24,36 @@ describe User do
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
   end
+
+  it "should accept valid email addresses" do
+    valid_addresses = %w{user@foo.com THE_USER@foo.bar.org
+                         first.last@foo.jp}
+    valid_addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should reject invalid email addresses" do
+    invalid_addresses = %w{user@foo,com user_at_foo.org example.user@foo.}
+    invalid_addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate email addresses" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+
+  # Email addresses are case-insensitive, so make sure
+  # if foo@bar.com exists, then FOO@bar.com can't sign up.
+  it "should reject email addresses identical up to case" do
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
 end
