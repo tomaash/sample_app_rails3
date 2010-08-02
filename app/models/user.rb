53ac5bacd0103abb221e7f82578b2e079f3e87d3
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-02 13:14:36 -0700

    Add user authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,13 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+    # Handle password mismatch implicitly (returns nil)
+  end
+
   private
 
     def encrypt_password
