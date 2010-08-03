9b90e5c6ddb9f8520b22e3ca7599dd90d7f35133
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-02 22:58:50 -0700

    Sign in user when they sign up

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -118,6 +118,11 @@ describe UsersController do
         post :create, :user => @attr
         flash[:success].should =~ /Welcome to the sample app/i
       end
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end # success
   end # POST 'create'
 end
