7b97b729c69959d2dbceed6d3c439e4112cf5df2
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-03 02:22:45 -0700

    Add User update action (with spec)

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -146,4 +146,54 @@ describe UsersController do
                                          :content => "change")
     end
   end # GET 'edit'
+
+  describe "PUT 'update'" do
+    before(:each) do
+      @user = Factory(:user)
+      test_sign_in(@user)
+    end
+
+    describe "failure" do
+      before(:each) do
+        @invalid_attr = { :email => "", :name => "" }
+      end
+
+      it "should render the 'edit' page" do
+        put :update, :id => @user, :user => @invalid_attr
+        response.should render_template('edit')
+      end
+
+      it "should have the right title" do
+        put :update, :id => @user, :user => @invalid_attr
+        response.should have_selector("title", :content => "Edit user")
+      end
+    end # failure
+
+    describe "success" do
+      before(:each) do
+        @attr = { :name => "New Name",
+                  :email => "user@example.org",
+                  :password => "foobarbaz",
+                  :password_confirmation => "foobarbaz" }
+      end
+
+      it "should change the user's attribute" do
+        put :update, :id => @user, :user => @attr
+        user = assigns(:user)
+        @user.reload
+        @user.name.should  == user.name
+        @user.email.should == user.email
+      end
+
+      it "should redirect to the user show page" do
+        put :update, :id => @user, :user => @attr
+        response.should redirect_to(user_path(@user))
+      end
+
+      it "should have a flash message" do
+        put :update, :id => @user, :user => @attr
+        flash[:success].should =~ /updated/
+      end
+    end # success
+  end # PUT "update"
 end
