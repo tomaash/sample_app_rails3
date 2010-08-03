389295d03b1426f41330b4d74922e5e29e792f9b
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-02 22:17:58 -0700

    Fill out sessions#create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.authenticate(params[:session][:email],
+                             params[:session][:password])
+    if user.nil?
+      # Create error message and re-render signin page
+      flash.now[:error] = "Invalid email/password combination."
+      @title = "Sign in"
+      render 'new'
+    else
+      # Sign user in and redirect to user's show page
+    end
   end
 
   def destroy
