802b564d00753b37a7f9edd76d7d77a028749932
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-04 14:21:50 -0700

    Add belongs_to for relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,6 @@
 class Relationship < ActiveRecord::Base
   attr_accessible :followed_id
+
+  belongs_to :follower, :foreign_key => :follower_id, :class_name => "User"
+  belongs_to :followed, :foreign_key => :followed_id, :class_name => "User"
 end
