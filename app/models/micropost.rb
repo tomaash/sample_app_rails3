09201d84ba038bdd657ab14405e29dbd0163da80
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-03 22:16:50 -0700

    Add micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,5 +3,9 @@ class Micropost < ActiveRecord::Base
 
   belongs_to :user
 
+  validates :content, :length => { :maximum => 140 },
+                      :presence => true
+  validates :user_id, :presence => true
+
   default_scope :order => 'microposts.created_at DESC'
 end
