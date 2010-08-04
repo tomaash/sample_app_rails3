faef8b9ed69f0e66a8d926c86abfda9e28722f94
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-04 11:13:25 -0700

    Add tests for (nonexistent) microposts controller

---
+++ spec/controllers/microposts_controller_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe MicropostsController do
+  render_views
+
+  describe "accesss control" do
+    it "should deny access to 'create'" do
+      post :create
+      response.should redirect_to(signin_path)
+    end
+
+    it "should deny access to 'destroy'" do
+      post :destroy, :id => 1
+      response.should redirect_to(signin_path)
+    end
+  end # access control
+end
