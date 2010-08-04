a12e460a14c6c44d9d1af0e0175d35aa89aeb959
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-04 14:13:39 -0700

    User has_many :relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -185,4 +185,15 @@ describe User do
       end
     end # status feed
   end # micropost associations
+
+  describe "relationships" do
+    before(:each) do
+      @user = User.create!(@attr)
+      @followed = Factory(:user)
+    end
+
+    it "should have a relationships method" do
+      @user.should respond_to(:relationships)
+    end
+  end
 end
