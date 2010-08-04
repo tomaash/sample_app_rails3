a4864af7c9eb8efd9e30f9f68dad0263d02cba92
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-04 14:24:49 -0700

    follower_id and followed_id can't be blank

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,7 @@ class Relationship < ActiveRecord::Base
 
   belongs_to :follower, :foreign_key => :follower_id, :class_name => "User"
   belongs_to :followed, :foreign_key => :followed_id, :class_name => "User"
+
+  validates :follower_id, :presence => true
+  validates :followed_id, :presence => true
 end
