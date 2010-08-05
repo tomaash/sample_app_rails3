f3d13793d68251dbc3c16d63ee9e92a58364c24d
Gabe Berke-Williams <gbw@brandeis.edu>
2010-08-04 23:27:43 -0700

    Add controller actions for creating/destroying Relationships

---
+++ spec/controllers/relationships_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe RelationshipsController do
+  describe "access control" do
+    it "should require signin for create" do
+      post :create
+      response.should redirect_to(signin_path)
+    end
+
+    it "should require signin for destroy" do
+      delete :destroy
+      response.should redirect_to(signin_path)
+    end
+  end # access control
+
+  describe "POST 'create'" do
+    before(:each) do
+      @user = test_sign_in(Factory(:user))
+      @followed = Factory(:user, :email => Factory.next(:email))
+    end
+
+    it "should create a relationship" do
+      lambda do
+        post :create, :relationship => { :followed_id => @followed }
+        response.should be_redirect
+      end.should change(Relationship, :count).by(1)
+    end
+  end # POST 'create'
+
+  describe "DELETE 'destroy'" do
+    before(:each) do
+      @user = test_sign_in(Factory(:user))
+      @followed = Factory(:user, :email => Factory.next(:email))
+      @user.follow!(@followed)
+      @relationship = @user.relationships.find_by_followed_id(@followed)
+    end
+
+    it "should destroy a relationship" do # !
+      lambda do
+        delete :destroy, :id => @relationship
+        response.should be_redirect
+      end.should change(Relationship, :count).by(-1)
+    end
+  end # DELETE 'destroy'
+end
